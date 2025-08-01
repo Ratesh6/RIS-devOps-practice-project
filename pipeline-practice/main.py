0f1049190b0d07f2c12e0d78a8071c36390ac094
Ratesh K <rateshk123@gmail.com>
2025-08-01 11:17:08 +0530

    Commit By ratesh

--- pipeline-practice/main.py
+++ pipeline-practice/main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello from DevOps Engineering Team!"
+    return "Hello from DevOps Engineering Team! by Ratesh k"
 
 
 if __name__ == '__main__':
